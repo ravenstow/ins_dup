5d0093f67634e9ad2e8ff7a36f2be1f10afcdf90
Ravenstow <wvyu7231@gmail.com>
2022-12-16 16:22:14 +0800

    - instagram主顏色佈置

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const royalBlue = Color(0xFF405DE6);
+const blue = Color(0xFF5B51D8);
+const purple = Color(0xFF833AB4);
+const darkPink = Color(0xFFC13584);
+const purpleRed = Color(0xFFE1306C);
+const red = Color(0xFFFD1D1D);
+const darkOrange = Color(0xFFF56040);
+const orange = Color(0xFFF77737);
+const yellow = Color(0xFFFCAF45);
+const lightYellow = Color(0xFFFFDC80);
